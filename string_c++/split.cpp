c4d734e9097aea2b026d3bb605c1b0d731f01b16
alfie-nsugh <40834779+alfie-nsugh@users.noreply.github.com>
2018-07-05 17:24:07 +0200

    Add files via upload

--- string_c++/split.cpp
+++ string_c++/split.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cctype>
+#include <algorithm>
 
 using std::string;
 using std::vector;
@@ -8,7 +9,7 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-vector<string> split(const string& s)
+vector<string> split_deprecated(const string& s)
 {
 	vector<string> ret;
 	typedef string::size_type string_size;
@@ -36,3 +37,30 @@ vector<string> split(const string& s)
 	return ret;
 }
 
+bool space(char c){
+	return isspace(c);
+}
+
+bool not_space(char c){
+	return !isspace(c);
+}
+
+vector<string> split(const string& str){
+		typedef string::const_iterator iter;
+		vector<string> ret;
+
+		iter i = str.begin();
+		while(i != str.end()){
+			//ignore leading blanks
+			i = find_if(i, str.end(), not_space);
+
+			//find end of next word
+			iter j = find_if(i, str.end(), space);
+
+			//copy the characters in [i,j)
+			if(i != str.end())
+				ret.push_back(string(i,j));
+			i = j;
+		}
+		return ret;
+}
